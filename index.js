b5c4d387246ea565339aa823f66aec77aa651298
fand <fand@gmork.in>
2016-01-29 20:04:26 +0900

    x

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 'use strict';
 
-const Nightmare = require('nightmare');
-const vo        = require('vo');
+const axios   = require('axios');
+const cheerio = require('cheerio');
 
-const run = function* (query) {
-  const n = Nightmare();
-  yield n.goto(`http://www.irasutoya.com/search?q=${query}`);
+function run (query) {
+  axios.get(`http://www.irasutoya.com/search?q=${query}`).then((html) => {
+    const $ = cheerio.load(html);
+    return $('.boxim > a').map((i, e) => $(e).attr('href'));
+  })
+  .then((urls) => {
+    if (urls.length === 0) {
+      throw new Error('No illusts found');
+    }
 
-  const urls = yield n.evaluate((selector) => {
-    const as = document.querySelectorAll(selector);
-    return [].map.call(as, (a) => a.getAttribute('href'));
-  }, '.boxim > a');
+    const entryUrl = urls[Math.floor(Math.random() * urls.length)];
+    return axios.get(entryUrl);
+  })
+  .then((html) => {
+    const $ = cheerio.load(html);
+    const imageUrl = $('.entry > .separator > a').attr('href');
 
-  if (urls.length === 0) {
-    console.log('No illusts found');
-    yield n.end();
-  }
-
-  const entryUrl = urls[Math.floor(Math.random() * urls.length)];
-
-  const imageUrl = yield n.goto(entryUrl).evaluate((selector) => {
-    return document.querySelector(selector).getAttribute('href');
-  }, '.entry > .separator > a');
-
-  console.log(imageUrl);
-
-  yield n.end();
-};
-
-module.exports = function (query) {
-  vo(run)(query, (err) => {
-    if (err) { throw err; }
+    console.log(imageUrl);
+  })
+  .catch((e) => {
+    console.log(e);
   });
-};
+}
+
+module.exports = run;
